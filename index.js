b9fb63fcfabdb641262d18de6e35b5baa19d3b79
郑茂强 <maoqiang_zheng@fzzixun.com>
2021-06-30 18:31:58 +0800

    默认加载到webview

--- index.js
+++ index.js
@@ -1,31 +1,32 @@
-const { ipcRenderer } = require("electron");
+const { ipcRenderer } = require('electron');
 //订阅
-const subscribe = async (service, cb) => {
+global.subscribe = async (service, cb) => {
   ipcRenderer.removeAllListeners(service);
   ipcRenderer.on(service, cb);
   return () => ipcRenderer.removeAllListeners(service);
 };
 //发布
-const publish = async (service, json = {}) => {
+global.publish = async (service, json = {}) => {
   ipcRenderer.sendToHost(service, json);
 };
 // 取消订阅/取消监听
-const unsubscribe = async (service) => {
+global.unsubscribe = async (service) => {
   if (service) {
     ipcRenderer.removeAllListeners(service);
   } else {
     ipcRenderer.removeAllListeners();
+    global.initIcp();
   }
 };
 
 //响应
-const response = async (service, cb) => {
+global.response = async (service, cb) => {
   ipcRenderer.once(service, (event, json) => {
     cb(event, json);
   });
 };
 //请求
-const request = async (service, json, cb) => {
+global.request = async (service, json, cb) => {
   if (cb) {
     ipcRenderer.once(service, (event, json) => {
       return cb(event, json);
@@ -40,4 +41,3 @@ const request = async (service, json, cb) => {
     });
   }
 };
-export { subscribe, publish, unsubscribe, response, request };
